c5e154f5e36da11a5cdd19cbfdc4304eff25be97
Irfan Ghesani <irghesani@twilio.com>
2021-12-08 17:39:23 -0500

    sms.go works in a docker container with curl - stoc price function is commented out

--- sms.go
+++ sms.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"net/http"
 	"os"
 
-	"github.com/piquette/finance-go/quote"
-	"github.com/sirupsen/logrus"
 	twilio "github.com/twilio/twilio-go"
 	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
-func sms(message string) {
+func sms(w http.ResponseWriter, req *http.Request) {
 	client := twilio.NewRestClient()
 
 	params := &openapi.CreateMessageParams{}
 	params.SetTo(os.Getenv("TO_PHONE_NUMBER"))
 	params.SetFrom(os.Getenv("TWILIO_PHONE_NUMBER"))
-	params.SetBody(message)
+	params.SetBody("You requested a quote")
 
 	_, err := client.ApiV2010.CreateMessage(params)
 	if err != nil {
@@ -28,15 +26,18 @@ func sms(message string) {
 }
 
 func main() {
-	flag.Parse()
+	/*
+		flag.Parse()
 
-	if len(flag.Args()) == 0 {
-		logrus.Fatalf("Input one stock symbol", os.Args[0])
-	}
-	symbol := flag.Args()[0]
-	a, _ := quote.Get(symbol)
-	fmt.Printf(" --%v--\n", a.ShortName)
-	fmt.Printf("Current Price: $%v\n", a.Ask)
-	s := fmt.Sprintf("Current Price: $%v\n", a.Ask)
-	sms(s)
+		if len(flag.Args()) == 0 {
+			logrus.Fatalf("Input one stock symbol", os.Args[0])
+		}
+		symbol := flag.Args()[0]
+		a, _ := quote.Get(symbol)
+		fmt.Printf(" --%v--\n", a.ShortName)
+		fmt.Printf("Current Price: $%v\n", a.Ask)
+		s := fmt.Sprintf("Current Price: $%v\n", a.Ask)
+	*/
+	http.HandleFunc("/quote", sms)
+	http.ListenAndServe(":8090", nil)
 }
